a415396daba78cf2a5dd3637d0ef570ab77147b9
reski-id <pesan.reski@gmail.com>
2022-08-08 23:43:42 +0700

    Fitur Pets domain/pets data/model

---
+++ domain/pets.go
@@ -0,0 +1,43 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Pets struct {
+	ID          int
+	Name        string
+	Gender      string
+	Age         int
+	Color       string
+	Images      string
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type PetsUseCase interface {
+	AddPets(newPets Pets) (Pets, error)
+	GetAllP() ([]Pets, error)
+	UpPets(IDPets int, updateData Pets) (Pets, error)
+	DelPets(IDPets int) (bool, error)
+	GetSpecificPets(PetsID int) ([]Pets, error)
+}
+
+type PetsHandler interface {
+	InsertPets() echo.HandlerFunc
+	GetAllPets() echo.HandlerFunc
+	UpdatePets() echo.HandlerFunc
+	DeletePets() echo.HandlerFunc
+	GetPetsID() echo.HandlerFunc
+}
+
+type ProductData interface {
+	Insert(insertPets Pets) Pets
+	GetAll() []Pets
+	Update(IDPets int, updatedPets Pets) Pets
+	Delete(IDPets int) bool
+	GetPetsID(PetsID int) []Pets
+}
