662e67a91e0c0403ea9b89463c11be424338bf28
rizunadiva <rizuananm@gmail.com>
2022-08-18 10:12:16 +0700

    edit response get species

--- features/species/delivery/response.go
+++ features/species/delivery/response.go
@@ -3,18 +3,21 @@ package delivery
 import "petadopter/domain"
 
 type SpeciesResponse struct {
-	Species []string
+	ID      int    `json:"id"`
+	Species string `json:"species"`
 }
 
 func FromModel(data domain.Species) SpeciesResponse {
 	var res SpeciesResponse
+	res.ID = data.ID
+	res.Species = data.Species
 	return res
 }
 
-func FromModelToList(data []domain.Species) []string {
-	result := []string{}
+func FromModelToList(data []domain.Species) []SpeciesResponse {
+	result := []SpeciesResponse{}
 	for val := range data {
-		result = append(result, data[val].Species)
+		result = append(result, FromModel(data[val]))
 	}
 	return result
 }
