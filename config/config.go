3225c9ff09b66b2feeccee683cd6935d6a377c91
lukmanhafidz <lukmanhafidz40@gmail.com>
2022-08-19 09:01:02 +0700

    mark env

--- config/config.go
+++ config/config.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"strconv"
 	"sync"
-
-	"github.com/joho/godotenv"
 	//"github.com/joho/godotenv"
 )
 
@@ -40,11 +38,11 @@ func Getconfig() *Appconfig {
 func initConfig() *Appconfig {
 	var defaultconfig Appconfig
 
-	err := godotenv.Load("local.env")
-	if err != nil {
-		log.Println("cant load env file")
-		return nil
-	}
+	// err := godotenv.Load("local.env")
+	// if err != nil {
+	// 	log.Println("cant load env file")
+	// 	return nil
+	// }
 
 	defaultconfig.Username = os.Getenv("Username")
 	defaultconfig.Password = os.Getenv("Password")
