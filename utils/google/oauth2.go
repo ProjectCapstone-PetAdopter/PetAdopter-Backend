15bf07dc723bde1566a2fd0dfb9998d280533375
Lukman Hafidz <74646521+lukmanhafidz@users.noreply.github.com>
2022-08-29 15:55:17 +0700

    Update oauth2.go

--- utils/google/oauth2.go
+++ utils/google/oauth2.go
@@ -21,8 +21,8 @@ var (
 
 func InitOauth() *oauth2.Config {
 	googleOauthConfig = &oauth2.Config{
-		ClientID:     "1042598066184-em7kqkibstmmn9n0mjrbsj0vm15br88c.apps.googleusercontent.com",
-		ClientSecret: "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",
+		ClientID:     os.Getenv("ClientID"),
+		ClientSecret: os.Getenv("ClientSecret"),
 		Scopes: []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile",
 			"https://www.googleapis.com/auth/calendar.events", "https://www.googleapis.com/auth/calendar"},
 		Endpoint: google.Endpoint,
